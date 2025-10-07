33c0ce83660a98df7b0a0895977130d4f863b2d7
DevinMcDonald <devin1217@gmail.com>
2025-10-07 18:43:38 -0500

    create capturer object

--- src/capture.py
+++ src/capture.py
@@ -1,20 +1,37 @@
+import os
+
 import cv2
 
-cap = cv2.VideoCapture(0)
-frame_count = 0
 
-print("Press Enter to capture an image, or 'q' then Enter to quit.")
+class ImageCapturer:
+    TEMP_DIR: str = "/tmp"
+    IMAGE_DIR: str = f"{TEMP_DIR}/gallupStopMotion"
 
-while True:
-    cmd = input()
-    if cmd.lower() == "q":
-        break
+    def __init__(self):
+        if not os.path.isdir(self.IMAGE_DIR):
+            try:
+                print(f"Creating {self.IMAGE_DIR}")
+                os.mkdir(self.IMAGE_DIR)
+            except Exception as _:
+                print(f"Failed to create {self.IMAGE_DIR}")
 
-    ret, frame = cap.read()
-    if ret:
-        filename = f"frame_{frame_count:03d}.png"
-        cv2.imwrite(filename, frame)
-        print(f"Saved {filename}")
-        frame_count += 1
+    def capture(self):
+        print("Press Enter to capture an image, or 'q' then Enter to quit.")
+        frame_count: int = 0
+        cap = cv2.VideoCapture(0)
+        while True:
+            cmd = input()
+            if cmd.lower() == "q":
+                break
 
-cap.release()
+            ret, frame = cap.read()
+            if ret:
+                filename = f"{self.IMAGE_DIR}/frame_{frame_count:03d}.png"
+                succeeded: bool = cv2.imwrite(filename, frame)
+                if not succeeded:
+                    print(f"failed to save {filename}")
+                else:
+                    print(f"Saved {filename}")
+                frame_count += 1
+
+        cap.release()
